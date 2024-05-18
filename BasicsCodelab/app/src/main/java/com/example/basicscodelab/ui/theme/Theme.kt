a262089bc6b9b598a702f5256ff5f3c50fc25488
Shermannatrix <shermannatrix@gmail.com>
2024-05-18 22:44:30 +0800

    Finished the previous exercise, now moving on to the next one.

--- BasicsCodelab/app/src/main/java/com/example/basicscodelab/ui/theme/Theme.kt
+++ BasicsCodelab/app/src/main/java/com/example/basicscodelab/ui/theme/Theme.kt
@@ -9,18 +9,25 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    surface = Blue,
+    onSurface = Navy,
+    onPrimary = Chartreuse
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = LightBlue,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    surface = Blue,
+    onSurface  = Color.White,
+    onPrimary = Navy
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
