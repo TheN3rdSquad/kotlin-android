8daf8341012ce536a9b0bb1c09b94c34233e0133
Chen Weizhi <shermannatrix@gmail.com>
2024-05-18 15:28:06 +0800

    Just finished the first quick refresher. Time for something more in-depth.

--- MyAndroidKotlinRefresher/app/build.gradle.kts
+++ MyAndroidKotlinRefresher/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("androidx.navigation.safeargs")
 }
 
 android {
@@ -49,4 +50,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
